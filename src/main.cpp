4e7388e108c43a7edf2ccb6b9e47fb8d9eb88d87
Ho_fan <yohoho0987612345@gmail.com>
2023-12-18 09:07:10 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@ int main()
 {
     ShowDown show_down;
     std::shared_ptr<Player> ptrPlayer1;
+    std::shared_ptr<Player> ptrPlayer2;
+    std::shared_ptr<Player> ptrPlayer3;
+    std::shared_ptr<Player> ptrPlayer4;
+
     std::cout << "something" << std::endl;
     return 0;
 }
